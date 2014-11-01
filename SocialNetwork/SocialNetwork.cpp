ff5aa599484652dadee55738aec13f7a5b092d3a
aist2 <eric_lee.06@hotmail.com>
2014-11-01 21:28:39 +0800

    update

--- SocialNetwork/SocialNetwork.cpp
+++ SocialNetwork/SocialNetwork.cpp
@@ -1,11 +1,20 @@
 // SocialNetwork.cpp : Defines the entry point for the console application.
 //
+#include "graph.h"
 
-#include "stdafx.h"
-
-
-int _tmain(int argc, _TCHAR* argv[])
+int main()
 {
+	Graph g = Graph();
+
+	g.addEdge(1,2);
+
+	g.addEdge(2,3);
+
+	g.printEdges();
+
+	g.printVertices();
+
+	system("PAUSE");
 	return 0;
 }
 
